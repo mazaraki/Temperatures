3d82c206ce9b3a24c41b2d8c084b22afc1880ab0
dmazaraki <dimitramaz@yahoo.gr>
2019-02-06 16:14:04 +0100

    "Initial commit"

--- src/com/boo/Main.java
+++ src/com/boo/Main.java
@@ -1,14 +1,14 @@
 package com.boo;
 
-/**
- * @author dmazaraki
+/*
+  @author dmazaraki
  */
 
 import java.util.*;
 
 class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int[] temps;
 
         Scanner in = new Scanner(System.in);
