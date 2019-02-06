75a7946e119ae4f4e603e7fd6f2ee5be888a949f
dmazaraki <dimitramaz@yahoo.gr>
2019-02-06 16:10:37 +0100

    Initial commit

---
+++ src/com/boo/Main.java
@@ -0,0 +1,42 @@
+package com.boo;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Main {
+
+    public static void main(String args[]) {
+        int[] temps;
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        temps = new int[n];
+        for (int i = 0; i < n; i++) {
+            int t = in.nextInt();
+            temps[i] = t;
+        }
+
+        Arrays.sort(temps);
+        for (int item = 0; item < temps.length - 1; item++){
+            if (temps[item] == 0){
+                System.out.println(temps[item]);
+                break;
+            }
+            if((0 - Math.abs(temps[item])) == (0 - Math.abs(temps[item+1]))){
+                if (temps[item] > 0){
+                    System.out.println(temps[item]);
+                    break;
+                } else {
+                    System.out.println(temps[item+1]);
+                    break;
+                }
+            }
+        }
+    }
+}
