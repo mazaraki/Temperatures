622a0a50611877d580848a6656bb53f7f646707e
dmazaraki <dimitramaz@yahoo.gr>
2019-02-06 16:13:35 +0100

    "Initial commit"

--- src/com/boo/Main.java
+++ src/com/boo/Main.java
@@ -1,14 +1,11 @@
 package com.boo;
 
-import java.lang.reflect.Array;
-import java.util.*;
-import java.io.*;
-import java.math.*;
-
 /**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+ * @author dmazaraki
+ */
+
+import java.util.*;
+
 class Main {
 
     public static void main(String args[]) {
